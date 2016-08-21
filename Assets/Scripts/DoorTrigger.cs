c5329060287e74dbfec0e08ac0b2d8c2f6802781
kkostenkov <kkostenkov@gmail.com>
2016-08-21 12:07:50 +0300

    import (Sci-fi Door)

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTrigger : MonoBehaviour {
+    public Animation doorAnimation;
+    private bool isOpen = false;
+
+    public void Open()
+    {
+        if (isOpen) return;
+        doorAnimation.Play("open");
+    }
+}
