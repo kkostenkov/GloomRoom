361d129c318bb4dbd4680ab769e89c909426f24b
kkostenkov <kkostenkov@gmail.com>
2016-08-24 23:53:02 +0300

    fix (TouchChargeDealer) don't damage when dead

--- Assets/Scripts/Enemies/TouchChargeDealer.cs
+++ Assets/Scripts/Enemies/TouchChargeDealer.cs
@@ -2,9 +2,30 @@
 using System.Collections;
 
 public class TouchChargeDealer : MonoBehaviour {
+    private bool isAlive = true;
+    ChargeHolder chargeHolder;
+
+    void OnEnable()
+    {
+        chargeHolder = GetComponent<ChargeHolder>();
+        if (chargeHolder != null)
+        {
+            chargeHolder.Overcharged += OnOvercharge;
+        }
+    }
+    void OnDisable()
+    {
+        if (chargeHolder != null) chargeHolder.Overcharged -= OnOvercharge;
+    }
+
+    private void OnOvercharge()
+    {
+        isAlive = false;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (!isAlive) return;
         if (other.tag != Tags.Player) return;
         other.GetComponent<ChargeHolder>().StartChargeByTouch();
     }
